e5532820848f580f2b78f5f34d4e78e0b6a84036
Nathan Leach <nleach999@gmail.com>
2023-03-03 11:51:54 -0600

    alpine fs permissions fixed

--- sandbox/scripts/entrypoint.sh
+++ sandbox/scripts/entrypoint.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 set -eu
 
 trap $(exit $?) >> /dev/null 2>&1
 
-umask 0057
+umask 0047
 
-ScaResolver -c /resolver/Configuration.yml --scan-path /sandbox/code $([[ $# -eq 0 ]] && echo "-h" || :) $@
+ScaResolver $@ -c /sandbox/resolver/Configuration.yml --scan-path /sandbox/code $([[ $# -eq 0 ]] && echo "-h" || :)
