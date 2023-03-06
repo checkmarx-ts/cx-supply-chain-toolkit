86716e566cf62d01625661877736f257227ab304
Nathan Leach <nleach999@gmail.com>
2023-03-06 12:09:26 -0600

    ci/cd work

--- sandbox/tests/build-tests-linux-amd64.sh
+++ sandbox/tests/build-tests-linux-amd64.sh
@@ -56,59 +56,59 @@ testBuildRedhatUbi9MinimalSuccess()
     assertEquals 0 $?
 }
 
-# testBuildRedhatUbi8Success()
-# {
-#     $DOCKER_BUILD_PREFIX -t test:tag --build-arg BASE=redhat/ubi8:latest --target=resolver-redhat ..
-#     assertEquals 0 $?
-# }
+testBuildRedhatUbi8Success()
+{
+    $DOCKER_BUILD_PREFIX -t test:tag --build-arg BASE=redhat/ubi8:latest --target=resolver-redhat ..
+    assertEquals 0 $?
+}
 
-# testBuildRedhatUbi8MinimalSuccess()
-# {
-#     $DOCKER_BUILD_PREFIX -t test:tag --build-arg BASE=redhat/ubi8-minimal:latest --target=resolver-redhat ..
-#     assertEquals 0 $?
-# }
+testBuildRedhatUbi8MinimalSuccess()
+{
+    $DOCKER_BUILD_PREFIX -t test:tag --build-arg BASE=redhat/ubi8-minimal:latest --target=resolver-redhat ..
+    assertEquals 0 $?
+}
 
-# testBuildBuildpackDepsKineticSuccess()
-# {
-#     $DOCKER_BUILD_PREFIX -t test:tag --build-arg BASE=buildpack-deps:kinetic --target=resolver-debian ..
-#     assertEquals 0 $?
-# }
+testBuildBuildpackDepsKineticSuccess()
+{
+    $DOCKER_BUILD_PREFIX -t test:tag --build-arg BASE=buildpack-deps:kinetic --target=resolver-debian ..
+    assertEquals 0 $?
+}
 
-# testBuildBuildpackDepsSidSuccess()
-# {
-#     $DOCKER_BUILD_PREFIX -t test:tag --build-arg BASE=buildpack-deps:sid --target=resolver-debian ..
-#     assertEquals 0 $?
-# }
+testBuildBuildpackDepsSidSuccess()
+{
+    $DOCKER_BUILD_PREFIX -t test:tag --build-arg BASE=buildpack-deps:sid --target=resolver-debian ..
+    assertEquals 0 $?
+}
 
-# testBuildEclipseTemurinSuccess()
-# {
-#     $DOCKER_BUILD_PREFIX -t test:tag --build-arg BASE=eclipse-temurin:latest --target=resolver-debian ..
-#     assertEquals 0 $?
-# }
+testBuildEclipseTemurinSuccess()
+{
+    $DOCKER_BUILD_PREFIX -t test:tag --build-arg BASE=eclipse-temurin:latest --target=resolver-debian ..
+    assertEquals 0 $?
+}
 
-# testBuildAmazonCorretto8AmazonLinuxSuccess()
-# {
-#     $DOCKER_BUILD_PREFIX -t test:tag --build-arg BASE=amazoncorretto:8 --target=resolver-amazon ..
-#     assertEquals 0 $?
-# }
+testBuildAmazonCorretto8AmazonLinuxSuccess()
+{
+    $DOCKER_BUILD_PREFIX -t test:tag --build-arg BASE=amazoncorretto:8 --target=resolver-amazon ..
+    assertEquals 0 $?
+}
 
-# testBuildAmazonCorretto11AmazonLinuxSuccess()
-# {
-#     $DOCKER_BUILD_PREFIX -t test:tag --build-arg BASE=amazoncorretto:11 --target=resolver-amazon ..
-#     assertEquals 0 $?
-# }
+testBuildAmazonCorretto11AmazonLinuxSuccess()
+{
+    $DOCKER_BUILD_PREFIX -t test:tag --build-arg BASE=amazoncorretto:11 --target=resolver-amazon ..
+    assertEquals 0 $?
+}
 
-# testBuildAmazonCorretto8AlpineSuccess()
-# {
-#     $DOCKER_BUILD_PREFIX -t test:tag --build-arg BASE=amazoncorretto:8-alpine3.14 --target=resolver-alpine ..
-#     assertEquals 0 $?
-# }
+testBuildAmazonCorretto8AlpineSuccess()
+{
+    $DOCKER_BUILD_PREFIX -t test:tag --build-arg BASE=amazoncorretto:8-alpine3.14 --target=resolver-alpine ..
+    assertEquals 0 $?
+}
 
-# testBuildAmazonCorretto11AlpineSuccess()
-# {
-#     $DOCKER_BUILD_PREFIX -t test:tag --build-arg BASE=amazoncorretto:11-alpine3.14 --target=resolver-alpine ..
-#     assertEquals 0 $?
-# }
+testBuildAmazonCorretto11AlpineSuccess()
+{
+    $DOCKER_BUILD_PREFIX -t test:tag --build-arg BASE=amazoncorretto:11-alpine3.14 --target=resolver-alpine ..
+    assertEquals 0 $?
+}
 
 
 . ./shunit2-2.1.8/shunit2
