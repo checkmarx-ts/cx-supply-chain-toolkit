9a8f63d0102488126fa0f6d2b726ab08e831afa7
Nathan Leach <nleach999@gmail.com>
2023-03-06 12:52:16 -0600

    execution unit tests

--- sandbox/tests/execute-tests-linux-amd64.sh
+++ sandbox/tests/execute-tests-linux-amd64.sh
@@ -17,7 +17,7 @@ tearDown()
 
 oneTimeSetUp() {
     git clone https://github.com/checkmarx-ltd/cx-flow.git cxflow
-    $DOCKER_BUILD_PREFIX -t test:gradle --build-arg BASE=gradle:8-jdk11-alpine --target=resolver-alpine ..
+    $DOCKER_BUILD_PREFIX -t test --build-arg BASE=gradle:8-jdk11-alpine --target=resolver-alpine ..
    
 }
 
@@ -26,16 +26,16 @@ oneTimeTearDown() {
 }
 
 testNoArgsShowsHelp() {
-    $DOCKER_RUN_PREFIX test:gradle > output/out.txt
+    $DOCKER_RUN_PREFIX test > output/out.txt
     EXEC_RESULT=$?
     assertTrue 0 "[ $EXEC_RESULT -eq 0 -a $(wc -l output/out.txt | cut -d ' ' -f1) -gt 1 ]"
 }
 
 testHelpSameAsNoArgs() {
-    $DOCKER_RUN_PREFIX test:gradle > output/noargs_out.txt
+    $DOCKER_RUN_PREFIX test > output/noargs_out.txt
     EXEC_RESULT_NOARGS=$?
 
-    $DOCKER_RUN_PREFIX test:gradle help > output/args_out.txt
+    $DOCKER_RUN_PREFIX test help > output/args_out.txt
     EXEC_RESULT_ARGS=$?
 
     assertTrue 0 "[ $EXEC_RESULT_NOARGS -eq $EXEC_RESULT_ARGS -a $(wc -l output/noargs_out.txt | cut -d ' ' -f1) -eq $(wc -l output/args_out.txt | cut -d ' ' -f1) ]"
